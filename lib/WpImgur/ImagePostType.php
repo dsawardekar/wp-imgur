31a1885de3971afb94e3af023a340f628b863eff
Darshan Sawardekar <darshan@sawardekar.org>
2014-05-28 12:20:27 +0530

    Implements ImagePostType api

--- lib/WpImgur/ImagePostType.php
+++ lib/WpImgur/ImagePostType.php
@@ -10,6 +10,70 @@ class ImagePostType {
     );
   }
 
+  function create($postName, $content) {
+    $post = array(
+      'post_type'    => $this->getName(),
+      'post_name'    => $this->toSlug($postName),
+      'post_content' => $this->toJSON($content),
+      'post_status'  => 'publish'
+    );
+
+    return wp_insert_post($post, true);
+  }
+
+  function find($postName) {
+    $options = array(
+      'post_type'      => $this->getName(),
+      'name'           => $this->toSlug($postName),
+      'paged'          => 1,
+      'posts_per_page' => 1
+    );
+
+    $query = new \WP_Query($options);
+    $posts = $query->get_posts();
+
+    if (count($posts) === 1) {
+      return $posts[0];
+    } else {
+      return false;
+    }
+  }
+
+  function findBy($postNames, $pageNum = 1, $pageSize = 25) {
+    $slugs = array_map(
+      array($this, 'toSlug'), $postNames
+    );
+
+    $options = array(
+      'post_type'      => $this->getName(),
+      'names'          => $slugs,
+      'paged'          => $pageNum,
+      'posts_per_page' => $pageSize
+    );
+
+    $query = new \WP_Query($options);
+    return $query->get_posts();
+  }
+
+  function findImages() {
+    global $wpdb;
+    $sql = <<<SQL
+    SELECT id from $wpdb->posts
+    WHERE
+      post_type = 'attachment' AND
+      post_mime_type LIKE 'image/%';
+SQL;
+
+    $images = $wpdb->get_results($sql, ARRAY_N);
+    $ids = array();
+
+    foreach ($images as $image) {
+      array_push($ids, $image[0]);
+    }
+
+    return $ids;
+  }
+
   function getName() {
     return 'imgur_image';
   }
@@ -25,6 +89,15 @@ class ImagePostType {
       'query_var'           => false,
       'can_export'          => true
     );
+  }
+
+  /* helpers */
+  function toSlug($postName) {
+    return sanitize_title_with_dashes($postName, null, 'save');
+  }
+
+  function toJSON($content) {
+    return json_encode($content);
   }
 
 }
--- test/WpImgur/ImagePostTypeTest.php
+++ test/WpImgur/ImagePostTypeTest.php
@@ -46,4 +46,62 @@ class ImagePostTypeTest extends \WP_UnitTestCase {
     $this->assertTrue(post_type_exists('imgur_image'));
   }
 
+  function test_it_can_convert_post_name_to_slug() {
+    $actual = $this->postType->toSlug('Hello World');
+    $this->assertEquals('hello-world', $actual);
+  }
+
+  function test_it_can_convert_post_name_with_accented_characters_to_slug() {
+    $actual = $this->postType->toSlug('mûr pouvait être écrit mëur');
+    $this->assertEquals('m%c3%bbr-pouvait-%c3%aatre-%c3%a9crit-m%c3%abur', $actual);
+  }
+
+  function test_it_can_store_image_details() {
+    $this->postType->register();
+
+    $content = array('foo' => 'bar');
+    $result = $this->postType->create('an image', $content);
+
+    $this->assertInternalType('int', $result);
+  }
+
+  function test_it_can_find_stored_image() {
+    $this->postType->register();
+    $content = array('foo' => 'bar');
+    $this->postType->create('an image', $content);
+
+    $image = $this->postType->find('an image');
+    $json = $image->post_content;
+
+    $this->assertEquals(json_decode($json, true), $content);
+  }
+
+  function test_it_wont_find_missing_image() {
+    $post = $this->postType->find('an image');
+    $this->assertFalse($post);
+  }
+
+  function test_it_can_find_list_of_stored_images_by_post_names() {
+    $this->postType->register();
+    $this->postType->create('image 1', array('foo' => 1));
+    $this->postType->create('image 2', array('foo' => 2));
+    $this->postType->create('image 3', array('foo' => 3));
+
+    $images = $this->postType->findBy(array('image-1', 'image-2', 'image-3'));
+    $this->assertEquals(3, count($images));
+  }
+
+  function test_it_can_find_media_attachment_images() {
+    $meta = array(
+      'post_mime_type' => 'image/jpeg',
+      'post_type' => 'attachment'
+    );
+    $this->factory->attachment->create_object('image-1', 1, $meta);
+    $this->factory->attachment->create_object('image-2', 2, $meta);
+    $this->factory->attachment->create_object('image-3', 3, $meta);
+
+    $images = $this->postType->findImages();
+    $this->assertEquals(3, count($images));
+  }
+
 }
