1dd22e0d276c8840906b23d167fc1064f1e694c0
Darshan Sawardekar <darshan@sawardekar.org>
2014-08-07 15:53:45 +0530

    Updates arrowVersion in main plugin file.

--- wp-imgur.php
+++ wp-imgur.php
@@ -14,7 +14,7 @@ require_once(__DIR__ . '/vendor/dsawardekar/arrow/lib/Arrow/ArrowPluginLoader.ph
 function wp_imgur_main() {
   $options = array(
     'plugin' => 'WpImgur\Plugin',
-    'arrowVersion' => '1.6.0'
+    'arrowVersion' => '1.8.0'
   );
 
   ArrowPluginLoader::load(__FILE__, $options);
