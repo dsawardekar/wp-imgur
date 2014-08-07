8244b4f6010e8b8083adc1e6a10a842db45cfac9
Darshan Sawardekar <darshan@sawardekar.org>
2014-08-07 16:50:43 +0530

    Removes development files [ci-skip]

--- js/wp-imgur/app/ext/ember_i18n.js
+++
@@ -1,6 +0,0 @@
-import Ember from 'ember';
-
-Ember.I18n.translations = window.wp_imgur;
-Ember.I18n.I18N_COMPILE_WITHOUT_HANDLEBARS = true;
-
-export default Ember.I18n;
--- js/wp-imgur/tests/helpers/resolver.js
+++
@@ -1,9 +0,0 @@
-import Resolver from 'ember/resolver';
-
-var resolver = Resolver.create();
-
-resolver.namespace = {
-  modulePrefix: 'wp-imgur'
-};
-
-export default resolver;
